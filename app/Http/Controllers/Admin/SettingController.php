c985e98fe7071654ac3ba47474bb1b1244bd345f
bhagyeshchavda <bhagyesh@glorywebsdev.com>
2024-06-12 18:36:18 +0530

    general setting add

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -32,6 +32,12 @@ class SettingController extends Controller {
     public function updateSettings(StoreGeneralSettings $request) {
         parse_str($request->formData, $searcharray);
         $data = array(
+            "gs_front_ficon"    => $searcharray['gs_front_ficon'],
+            "gs_front_logo"     => $searcharray['gs_front_logo'],
+            "gs_email_logo"     => $searcharray['gs_email_logo'],
+            "front_email"       => $searcharray['front_email'],
+            "front_phone"       => $searcharray['front_phone'],
+            "front_website"     => $searcharray['front_website'],
             "gs_ficon"          => $searcharray['gs_ficon'],
             "gs_sidebaricon"    => $searcharray['gs_sidebaricon'],
             "gs_adminlogo"      => $searcharray['gs_adminlogo'],
--- resources/views/admin/setting/index.blade.php
+++ resources/views/admin/setting/index.blade.php
@@ -35,6 +35,10 @@
             transform: rotate(360deg);
         }
     }
+    .nav-tabs.flex-column {
+        border-top: 1px solid #dee2e6;
+        border-radius: .25rem;
+    }
 </style>
 <div class="content-wrapper">
     <div class="content-header">
@@ -81,12 +85,152 @@
                                 <div class="row">
                                     <div class="col-5 col-sm-3">
                                         <div class="nav flex-column nav-tabs h-100" id="vert-tabs-tab" role="tablist" aria-orientation="vertical">
-                                            <a class="nav-link active" id="vert-tabs-admin-tab" data-toggle="pill" href="#vert-tabs-admin" role="tab" aria-controls="vert-tabs-admin" aria-selected="false">Admin setting</a>
+                                            <a class="nav-link active" id="vert-tabs-front-tab" data-toggle="pill" href="#vert-tabs-front" role="tab" aria-controls="vert-tabs-front" aria-selected="false">Front setting</a>
+                                            <a class="nav-link" id="vert-tabs-admin-tab" data-toggle="pill" href="#vert-tabs-admin" role="tab" aria-controls="vert-tabs-admin" aria-selected="false">Admin setting</a>
                                         </div>
                                     </div>
                                     <div class="col-7 col-sm-9">
                                         <div class="tab-content" id="vert-tabs-tabContent">
-                                            <div class="tab-pane text-left fade active show" id="vert-tabs-admin" role="tabpanel" aria-labelledby="vert-tabs-general-tab">
+                                            <div class="tab-pane text-left fade active show" id="vert-tabs-front" role="tabpanel" aria-labelledby="vert-tabs-general-tab">
+                                                <div class="card">
+                                                    <div class="card-body">
+                                                        <div class="form-group row">  
+                                                            <div class="col-sm-6 col-md-6">
+                                                                <label class="col-form-label">Front Favicon</label>
+                                                                <div class="form-image text-center @if(isset($data['gs_front_ficon']) && $data['gs_front_ficon'] != '') previewing  @endif">
+                                                                    <a href="javascript:void(0)" class="image-clear" style="display: none"><i
+                                                                            class="fa fa-times-circle fa-2x"></i>
+                                                                    </a>
+                                                                    @if(!empty(isset($data['gs_front_ficon'])) && $data['gs_front_ficon'] != '')
+                                                                        <a href="javascript:void(0)" class="image-clear">
+                                                                            <i class="fa fa-times-circle fa-2x"></i>
+                                                                        </a>
+                                                                        <input type="hidden" name="gs_front_ficon" class="input-path" value="{{$data['gs_front_ficon']}}"/>      
+                                                                    @else
+                                                                        <input type="hidden" name="gs_front_ficon" class="input-path" value=""/>      
+                                                                    @endif
+                                                                    <div class="dropify-preview image-hidden" @if ($data) @if(isset($data['gs_front_ficon']) && $data['gs_front_ficon'] != '') style="display: block" @endif @endif>
+                                                                        <span class="dropify-render">
+                                                                            @if (isset($data['gs_front_ficon']))
+                                                                                @if ($data['gs_front_ficon'])
+                                                                                    <img src="{{ asset($data['gs_front_ficon']) }}" alt="fevicon"/>
+                                                                                @endif
+                                                                            @endif
+                                                                        </span>
+                                                                        <div class="dropify-infos">
+                                                                            <div class="dropify-infos-inner">
+                                                                                <p class="dropify-filename"><span class="dropify-filename-inner"></span></p>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="icon-choose">
+                                                                        <i class="fas fa-cloud-upload-alt fa-5x"></i>
+                                                                        <p>Click here to select file</p>
+                                                                    </div>
+                                                                    <span class="text-danger error-text old_favicon_error"></span>
+                                                                </div>
+                                                            </div>  
+                                                            
+                                                            <div class="col-sm-6 col-md-6">
+                                                                <label class="col-form-label">Front Header Logo</label>
+                                                                <div class="form-image text-center  @if(isset($data['gs_front_logo']) && $data['gs_front_logo'] != '') previewing @endif">
+                                                                    <a href="javascript:void(0)" class="image-clear" style="display: none"><i
+                                                                            class="fa fa-times-circle fa-2x"></i>
+                                                                    </a>
+                                                                    @if(!empty($data) && isset($data['gs_front_logo']) && $data['gs_front_logo'] != '')
+                                                                        <a href="javascript:void(0)" class="image-clear">
+                                                                            <i class="fa fa-times-circle fa-2x"></i>
+                                                                        </a>
+                                                                        <input type="hidden" name="gs_front_logo" class="input-path" value="{{$data['gs_front_logo']}}"/>      
+                                                                    @else
+                                                                        <input type="hidden" name="gs_front_logo" class="input-path" value=""/>      
+                                                                    @endif
+                                                                    <div class="dropify-preview image-hidden" @if ($data) @if(isset($data['gs_front_logo']) && $data['gs_front_logo'] != '') style="display: block" @endif @endif>
+                                                                        <span class="dropify-render">
+                                                                            @if (isset($data['gs_front_logo']))
+                                                                                @if ($data['gs_front_logo'])
+                                                                                    <img src="{{ asset($data['gs_front_logo']) }}" alt="admin-logo"/>
+                                                                                @endif
+                                                                            @endif
+                                                                        </span>
+                                                                        <div class="dropify-infos">
+                                                                            <div class="dropify-infos-inner">
+                                                                                <p class="dropify-filename"><span class="dropify-filename-inner"></span></p>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="icon-choose">
+                                                                        <i class="fas fa-cloud-upload-alt fa-5x"></i>
+                                                                        <p>Click here to select file</p>
+                                                                    </div>
+                                                                    <span class="text-danger error-text old_favicon_error"></span>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-sm-12 col-md-12">
+                                                                <label class="col-form-label">Enter Email Header Logo</label>
+                                                                <div class="form-image text-center @if(isset($data['gs_front_logo']) && $data['gs_front_logo'] != '') previewing @endif">
+                                                                    <a href="javascript:void(0)" class="image-clear" style="display: none"><i
+                                                                            class="fa fa-times-circle fa-2x"></i>
+                                                                    </a>
+                                                                    @if(!empty($data) && isset($data['gs_email_logo']) && $data['gs_email_logo'] != '')
+                                                                        <a href="javascript:void(0)" class="image-clear">
+                                                                            <i class="fa fa-times-circle fa-2x"></i>
+                                                                        </a>
+                                                                        <input type="hidden" name="gs_email_logo" class="input-path" value="{{$data['gs_email_logo']}}"/>      
+                                                                    @else
+                                                                        <input type="hidden" name="gs_email_logo" class="input-path" value=""/>      
+                                                                    @endif
+                                                                    <div class="dropify-preview image-hidden" @if ($data) @if(isset($data['gs_front_logo']) && $data['gs_front_logo'] != '') style="display: block" @endif @endif>
+                                                                        <span class="dropify-render">
+                                                                            @if (isset($data['gs_email_logo']))
+                                                                                @if ($data['gs_email_logo'])
+                                                                                    <img src="{{ asset($data['gs_email_logo']) }}" alt="admin-logo"/>
+                                                                                @endif
+                                                                            @endif
+                                                                        </span>
+                                                                        <div class="dropify-infos">
+                                                                            <div class="dropify-infos-inner">
+                                                                                <p class="dropify-filename"><span class="dropify-filename-inner"></span></p>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="icon-choose">
+                                                                        <i class="fas fa-cloud-upload-alt fa-5x"></i>
+                                                                        <p>Click here to select file</p>
+                                                                    </div>
+                                                                    <span class="text-danger error-text old_favicon_error"></span>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-sm-4 col-md-4">
+                                                                <label class="col-form-label">Enter Header Email</label>
+                                                                @if(isset($data['front_email']) && $data['front_email'] != '')
+                                                                    <input class="form-control form-control-solid" value="{{$data['front_email']}}" type="text" name="front_email" placeholder="Enter Email Address" />
+                                                                @else
+                                                                    <input class="form-control form-control-solid" type="text" name="front_email" placeholder="Enter Email Address" />
+                                                                @endif
+                                                            </div>
+                                                            <div class="col-sm-4 col-md-4">
+                                                                <label class="col-form-label">Enter Header Phone</label>
+                                                                @if(isset($data['front_phone']) && $data['front_phone'] != '')
+                                                                    <input class="form-control form-control-solid" value="{{$data['front_phone']}}" type="text" name="front_phone" placeholder="Enter Phone" />
+                                                                @else
+                                                                    <input class="form-control form-control-solid" type="text" name="front_phone" placeholder="Enter Phone" />
+                                                                @endif
+                                                            </div>
+                                                            <div class="col-sm-4 col-md-4">
+                                                                <label class="col-form-label">Enter Header Website</label>
+                                                                @if(isset($data['front_website']) && $data['front_website'] != '')
+                                                                    <input class="form-control form-control-solid" value="{{$data['front_website']}}" type="text" name="front_website" placeholder="Enter Website" />
+                                                                @else
+                                                                    <input class="form-control form-control-solid" type="text" name="front_website" placeholder="Enter Website" />
+                                                                @endif
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>	 
+                                            </div>
+                                        
+                                            <div class="tab-pane text-left fade" id="vert-tabs-admin" role="tabpanel" aria-labelledby="vert-tabs-general-tab">
                                                 <div class="card">
                                                     <div class="card-body">
                                                         <div class="form-group row">  
